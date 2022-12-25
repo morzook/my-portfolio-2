3fed073c96543d3de7ddae0ea29853cedc091853
morzook <59170230+morzook@users.noreply.github.com>
2022-12-25 18:49:49 +0100

    new version

---
+++ script.js
@@ -0,0 +1,20 @@
+// const right = document.querySelector('.right');
+// const left = document.querySelector('.left');
+// const frame = document.querySelector('.project-frame');
+
+// right.addEventListener('click', moveRight);
+// function moveRight() {
+//   const maxScroll = frame.scrollWidth - frame.clientWidth;
+//   if (frame.scrollLeft < maxScroll) {
+//     frame.scrollLeft += 10;
+//     window.requestAnimationFrame(moveRight);
+//   }
+// }
+
+// left.addEventListener('click', moveLeft);
+// function moveLeft() {
+//   if (frame.scrollLeft > 0) {
+//     frame.scrollLeft -= 20;
+//     window.requestAnimationFrame(moveLeft);
+//   }
+// }
